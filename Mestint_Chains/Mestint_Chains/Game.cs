72c59dc5ac3c156b9d07ccd39670c53837edf680
zrobi0621 <zrobi0621@gmail.com>
2019-05-26 21:13:41 +0200

    Chains v0.0001

---
+++ Mestint_Chains/Mestint_Chains/Game.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mestint_Chains
+{
+    class Game
+    {
+        public List<int[]> originalBoard = new List<int[]>()
+        {
+            new int[]{0,0,0 },
+            new int[]{0,0,0,0 },
+            new int[]{0,0,0,0,0 },
+            new int[]{0,0,0,0 },
+            new int[]{0,0,0 },
+        };
+
+        public int[,] originalMatrix = new int[,]{
+            {-1,-1,0,0,0,-1},
+            {-1,0,0,0,0,-1},
+            {0,0,0,0,0,0},
+            {-1,0,0,0,0,-1},
+            {-1,-1,0,0,0,-1}
+        };
+        //int[] originalBoard = { 0, 1, 0, 0, 0, 0, 0, 0 };
+                              //0 -> empty, -1 - > wall
+        int  huPlayer = 1;    //black
+        int  aiPlayer = 2;    //white
+
+        int score;
+        List<int> emptyCells = new List<int>();
+
+        public int Minimax(int[] newBoard, int player)
+        {
+            emptyCells = GetEmptyCells();
+
+            if (Winning(newBoard, huPlayer))
+            {
+                return score = -10;
+            }
+            else if (Winning(newBoard, aiPlayer))
+            {                
+                return score = 10;
+            }
+            else if (emptyCells.Count == 0)
+            {
+                return score = 0;
+            }
+
+            Dictionary<int,int> moves = new Dictionary<int,int>();
+
+            for (int i = 0; i < emptyCells.Count; i++)
+            {                
+                int moveIndex;
+                int moveScore;
+
+                moveIndex = newBoard[emptyCells[i]];
+
+                newBoard[emptyCells[i]] = player;
+
+                if (player == aiPlayer)
+                {
+                    int result = Minimax(newBoard, huPlayer);
+                    moveScore = result;
+                }
+                else
+                {
+                    int result = Minimax(newBoard, aiPlayer);
+                    moveScore = result;
+                }
+                newBoard[emptyCells[i]] = moveIndex;
+
+                moves.Add(moveIndex, moveScore);
+            }
+
+            int bestMove = 0;
+
+            if (player == aiPlayer)
+            {
+                int bestScore = -10000;
+                for (int i = 0; i < moves.Count; i++)
+                {
+                    if (moves.ElementAt(i).Value > bestScore)
+                    {
+                        bestScore = moves.ElementAt(i).Value;
+                        bestMove = i;
+                    }
+                }
+            }
+            else
+            {
+                int bestScore = 10000;
+                for (int i = 0; i < moves.Count; i++)
+                {
+                    if (moves.ElementAt(i).Value < bestScore)
+                    {
+                        bestScore = moves.ElementAt(i).Value;
+                        bestMove = i;
+                    }
+                }
+            }
+            return moves.ElementAt(bestMove).Key;            
+        }
+
+        private bool Winning(object newBoard, object huPlayer)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        private List<int> GetEmptyCells()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ Mestint_Chains/Mestint_Chains/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Mestint_Chains
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int color = 0;
+        int player = 1;    //black
+        int opponent = 2;    //white
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            StackPanel myStackPanel = MyStackPanel;
+            Game game = new Game();
+
+            CreateBoard();            
+        }
+
+        void CreateBoard()
+        {
+            List<int[]> originalBoard = new List<int[]>()
+            {
+                new int[]{0,0,0,0 },
+                new int[]{0,0,0,0,0 },
+                new int[]{0,0,0,0,0,0 },
+                new int[]{0,0,0,0,0 },
+                new int[]{0,0,0,0 },
+            };
+
+            Grid[] grids = new Grid[5]
+            {
+                FirstRow,
+                SecondRow,
+                ThirdRow,
+                FourthRow,
+                FifthRow
+            };
+
+            for (int i = 0; i < grids.Length; i++)
+            {
+                int[] row = originalBoard[i];
+                for (int j = 0; j < row.Length; j++)
+                {
+                    Canvas canvas = new Canvas();
+                    canvas.MouseDown += Canvas_MouseDown;
+                    canvas.Background = Brushes.Wheat;
+                    Grid.SetRow(canvas, i);
+                    Grid.SetColumn(canvas, j);
+                    grids[i].Children.Add(canvas);
+                }
+            }
+        }
+
+        //***************** EVENTS *****************
+        private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            int r = Grid.GetRow(sender as Canvas);
+            int c = Grid.GetColumn(sender as Canvas);
+            Canvas senderCanvas = sender as Canvas;
+
+            if (color != 0 && senderCanvas.Children.Count == 0)
+            {
+                if (color == 1) //Player
+                {
+                    Circle newCircle = new Circle(17);
+                    newCircle.Draw(sender as Canvas, Brushes.Black);
+                }
+                else if (color == 2)    //Opponent
+                {
+                    Circle newCircle = new Circle(17);
+                    newCircle.Draw(sender as Canvas, Brushes.White);
+                }
+            }            
+        }
+
+        private void ChangeColor_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            if (button.Name == "BlackButton")
+            {
+                color = 1;  //Player
+            }
+            else if (button.Name == "WhiteButton")
+            {
+                color = 2;  //Opponent
+            }
+        }
+    }
+}
